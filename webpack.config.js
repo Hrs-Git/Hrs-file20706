ba53db0c8a6506c05461dec6c705a56980f79f0e
Hrs-Git <65592948+Hrs-Git@users.noreply.github.com>
2020-07-07 19:50:03 +0900

    add img

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,18 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|jpg)/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+              name: 'img/[name].[ext]',
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
