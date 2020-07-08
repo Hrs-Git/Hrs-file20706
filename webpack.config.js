0af8508393294efb2d3b5e84f1524ef35e622875
Hrs-Git <65592948+Hrs-Git@users.noreply.github.com>
2020-07-08 23:43:18 +0900

    put add

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { loader } = require('mini-css-extract-plugin');
 
 //エントリーポイントを取得
 
@@ -26,13 +27,27 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|jpg)/,
+        test: /\.(png|jpg|svg)/,
         use: [
           {
             loader: 'file-loader',
             options: {
               esModule: false,
               name: 'img/[name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.pug/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+          {
+            loader: 'pug-html-loader',
+            options: {
+              pretty: true,
             },
           },
         ],
@@ -44,7 +59,12 @@ module.exports = {
       filename: './stylestheets/main.css',
     }),
     new HtmlWebpackPlugin({
-      template: './src/templates/index.html',
+      template: './src/templates/index.pug',
+      filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/templates/access.pug',
+      filename: 'access.html',
     }),
     new CleanWebpackPlugin(),
   ],
